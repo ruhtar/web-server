9148be9c280083e87bf189147733e95291ad6dbd
arthur-amorim-bs <aamorim@sprintmind.com.br>
2025-01-31 21:57:15 -0300

    Creates the page route

--- WebServer/WebServer/Router.cs
+++ WebServer/WebServer/Router.cs
@@ -7,38 +7,41 @@ public class Router
 {
     public static async Task HandleAsync(HttpListenerContext context)
     {
-
         var request = context.Request;
         var response = context.Response;
 
         Log.Print(request);
         if (request.Url?.AbsolutePath == "/favicon.ico")
         {
-            var faviconPath = Path.Combine(Environment.CurrentDirectory, "icons8-favicon-94.png");
-
-            faviconPath = faviconPath.Replace("bin\\Debug\\net8.0\\", "");
-
-            if (File.Exists(faviconPath))
-            {
-                byte[] faviconBytes = File.ReadAllBytes(faviconPath);
-
-                response.ContentType = "image/x-icon";
-                response.ContentLength64 = faviconBytes.Length;
-
-                await response.OutputStream.WriteAsync(faviconBytes);
-                response.OutputStream.Close();
-            }
-            else
-            {
-                response.StatusCode = (int)HttpStatusCode.NotFound;
-            }
+            await HandleFavicon(response);
 
             return;
         }
 
-        var method = request.HttpMethod;
+        if (request.RawUrl == "/page")
+        {
+            string filePath = Path.Combine(Environment.CurrentDirectory, "index.html");
 
-        // We have a connection, do something...
+            filePath = FixAssetsPath(filePath);
+
+            if (File.Exists(filePath))
+            {
+                string htmlContent = await File.ReadAllTextAsync(filePath);
+                byte[] buffer = Encoding.UTF8.GetBytes(htmlContent);
+
+                response.ContentType = "text/html";
+                response.ContentLength64 = buffer.Length;
+                response.OutputStream.Write(buffer, 0, buffer.Length);
+            }
+            else
+            {
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                byte[] error = Encoding.UTF8.GetBytes("<h1>404 - Página não encontrada</h1>");
+                response.OutputStream.Write(error);
+            }
+
+            return;
+        }
 
         string responseString = "Hello World! " + DateTime.UtcNow.AddHours(-3).ToString();
         byte[] encoded = Encoding.UTF8.GetBytes(responseString);
@@ -46,5 +49,36 @@ public class Router
         context.Response.OutputStream.Write(encoded, 0, encoded.Length);
         context.Response.OutputStream.Close();
         return;
+    }
+
+    private static async Task HandleFavicon(HttpListenerResponse response)
+    {
+        var faviconPath = Path.Combine(Environment.CurrentDirectory, "favicon.png");
+
+        faviconPath = FixAssetsPath(faviconPath);
+
+        if (File.Exists(faviconPath))
+        {
+            byte[] faviconBytes = File.ReadAllBytes(faviconPath);
+
+            response.ContentType = "image/x-icon";
+            response.ContentLength64 = faviconBytes.Length;
+
+            await response.OutputStream.WriteAsync(faviconBytes);
+            response.OutputStream.Close();
+        }
+        else
+        {
+            response.StatusCode = (int)HttpStatusCode.NotFound;
+        }
+    }
+
+    private static string FixAssetsPath(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+            return filePath;
+
+        filePath = filePath.Replace("WebServer\\bin\\Debug\\net8.0", "wwwroot");
+        return filePath;
     }
 }
